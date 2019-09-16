3b444129cd9434e4825e46876bb5dfe5f43f4542
DuverneyCM <duverney.corrales@correounivalle.edu.co>
2019-09-16 16:19:59 -0500

    get lastDirection, posLastArrow and getArrow

--- traceback/traceback.c
+++ traceback/traceback.c
@@ -60,56 +60,86 @@ int main(int argc, char *argv[]) {
 		printf("dimLastLineSeqB = %d\n",dimLastLineSeqB);
 		printf("dimSeqB = %d\n",dimSeqB);
 	//	Get features of the Arrow files
+	int noBytesByRow = 2*noPEs/8, arrowRow[noBytesByRow];
 	sprintf(arrowFILEname, "%sp%d.bin", argv[4], noFile);
 	farrows =	fopen(arrowFILEname,"rb");
 		printf("File = %s\n",arrowFILEname);
 	int dimFileArrows = getFileSize(farrows);
-	int	lastDir = getLastDirection(farrows, dimFileArrows, noPEs);
 	int NoArrowRows = getNoArrowRows(dimFileArrows, noPEs);
+	int	dirArrow = getLastDirection(farrows, dimFileArrows, noPEs, arrowRow);
+	int posArrow = getPosLastArrow(farrows, dimFileArrows, noPEs, arrowRow);
+	int ARROW = getArrow(noPEs, arrowRow, posArrow, dirArrow);
 		printf("dimFileArrows = %d\n",dimFileArrows);
-		printf("lastDir = %d\n",lastDir);
 		printf("NoArrowRows = %d\n",NoArrowRows);
+		printf("dirArrow = %d\n",dirArrow);
+		printf("posArrow = %d\n",posArrow);
+		printf("ARROW = %d\n",ARROW);
+/*	
+	//////BUSCAR LA POSICIÓN DE LA FLECHA EN LA PRIMERA FILA DE FLECHAS (next row). SOLO DEBE HABER UNA FLECHA
+	currentFILA = currentFILA - 1; // next row
+	for (i=0; i<NoRegs; i++)
+		fila[i] = bufferArrows[i + currentFILA*NoRegs];
+	//fseek(farrows, -2*NoRegs*dimUInt, SEEK_CUR);
+	//fread(&fila,dimUInt,NoRegs,farrows); //Lee la fila desde el archivo
 	
+	int dupla = 0;
+	int noArrows = 0;
+	int posArrow = -1;
+	int ARROW = 0;
+	//int prueba = (dirH>>6) & 3;
+	//	printf("%8X\n",prueba);
+	for (i=0;i<NoRegs;i++){
+		for (j=0;j<dimUInt*8/2;j++){
+			dupla = (fila[i]>>2*j) & 3;
+			if (dupla != 0) {
+				noArrows++;
+				posArrow = i*8*dimUInt + 2*j;
+				ARROW = dupla; //ARROW = 0 @ ERROR, 1 @ UP, 2 @ LEFT, 3 @ DIAGONAL
+			}
+		}
+	}
+		//Si noArrows es cero, no hay flechas en la fila.
+		//Si es mayor a 1, hay varias flechas en la fila, lo cual no corresponde con la última fila de flechas
+		printf("ARROW = %d\n",ARROW);
+		printf("noArrows = %d\n",noArrows);
+		printf("posArrow = %d\n",posArrow);
+		
+	//definir variables de destino
+		int posSeqA = dimSeqA;
+		int posSeqB = dimSeqB;
+		int gapA = 0;
+		int gapB = 0;
+		int offsetPos;
+		int offsetFila;
+	
+	int packA, packB;
+		if (dimSeqA % dimPackSeq == 0) packA = dimSeqA / dimPackSeq;
+		else packA = dimSeqA / dimPackSeq + 1;
+		if (dimSeqB % dimPackSeq == 0) packB = dimSeqB / dimPackSeq;
+		else packB = dimSeqB / dimPackSeq + 1;
+		
+		//LEER SEQA Y SEQB POR PAQUETES DE TAMAÑO dimPackSeq. Primer paquete (este) es de residuo
+			fseek(fseqA, posFirstEnterA + 1 + dimPackSeq*(packA-1), SEEK_SET);
+			fread(&VseqA,dimSeqA%dimPackSeq,1,fseqA);
+			printf("packA = %d\n",packA);
+			printf("range packA = %d %d\n", dimPackSeq*(packA-1), (int)ftell(fseqA) - (posFirstEnterA + 1) );
+			packA--;
+			
+			fseek(fseqB, posFirstEnterB + 1 + dimPackSeq*(packB-1), SEEK_SET);
+			fread(&VseqB,dimSeqB%dimPackSeq,1,fseqB);
+			printf("packB = %d\n",packB);
+			printf("range packB = %d %d\n",dimPackSeq*(packB-1), (int)ftell(fseqB) - (posFirstEnterB + 1) );
+			packB--;
+		
+	int cnt = 0;
+	int flag = 0;
 
-/*
+
 	char dataIn1[dimPacket + 1];
 		unsigned int fila[NoRegs];
 		static unsigned int bufferArrows[NooRegs*NoPacketFile*(2*dimPacket)];
 		static char VseqA[dimPackSeq+1], VseqB[dimPackSeq+1];
 		static char VfnewseqA[dimPackSeq+1], VfnewseqB[dimPackSeq+1];
-
     int currentFILA; //lastPosMemory, firstPosMemory,
-
-
-
-	//////VERIFICAR QUE LA ÚLTIMA FILA CONTIENE LA DIRECCIÓN H/V DEL ÚLTIMO CICLO DEL KBAND
-	sprintf(arrowFILEname, "%sp%d.bin", argv[4], noFile);
-	farrows =	fopen(arrowFILEname,"rb");
-		printf("File = %s\n",arrowFILEname);
-		
-	fseek(farrows, 0, SEEK_END);
-	currentFILA = ftell(farrows)/(NoRegs*dimUInt); //pos next arrow
-	fseek(farrows, 0, SEEK_SET);
-	fread(&bufferArrows,dimUInt,currentFILA*NoRegs,farrows);
-	currentFILA = currentFILA - 1; // next row
-	
-	//fseek(farrows, -1*NoRegs*dimUInt, SEEK_CUR);
-	
-
-	int isHV = 0;
-	int dirHV = 0;
-	//fread(&fila,dimUInt,NoRegs,farrows); //Lee la fila desde el archivo
-	for (i=0; i<NoRegs; i++)
-		fila[i] = bufferArrows[i + currentFILA*NoRegs];
-	for (i=0;i<NoRegs;i++){
-		//printf("%X\n",fila[i]);
-		if (fila[i] == (unsigned int)dirH) isHV++;
-		if (fila[i] == (unsigned int)dirV) isHV--;
-	}
-		//si dirHV sigue siendo cero, error en el archivo
-	if (isHV == NoRegs) dirHV = 1;		// V
-	if (isHV == -NoRegs) dirHV = -1;	// H
-		printf("isHV = %d\n",dirHV);
-		printf("dirHV = %d\n",dirHV);
 */
 }
--- traceback/tracebackFunctions.h
+++ traceback/tracebackFunctions.h
@@ -8,7 +8,11 @@ int getPosFirstEnter(FILE *inputFile, int dimFile, int dimSearchWindow);
 int getLineSize(FILE *inputFile, int dimFile, int posFirstEnter, int dimSearchWindow);
 int getLastLineSize(int dimFile, int posFirstEnter, int dimLine);
 int getDimSeq(int dimFile, int posFirstEnter, int dimLine);
-int getLastDirection(FILE *inputFile, int dimFile, int noPEs);
+int getNoArrowRows(int dimFile, int noPEs);
+int getLastDirection(FILE *inputFile, int dimFile, int noPEs, int *arrowRow);
+int getPosLastArrow(FILE *inputFile, int dimFile, int noPEs, int *arrowRow);
+int getArrow(int noPEs, int *arrowRow, int posArrow, int dirArrow);
+
 void decoArrow(int* offsetPos, int* offsetFila, int* dirHV, int ARROW, int* posSeqA, int* posSeqB, int* gapA, int* gapB);
 
 
@@ -75,31 +79,63 @@ int getDimSeq(int dimFile, int posFirstEnter, int dimLine) {
 	return dimSeq;
 }
 
-int getLastDirection(FILE *inputFile, int dimFile, int noPEs) {
-	const int dirHcode = 1431655765, dirVcode = 2863311530;
-	int sizeValid = 0, readRow = 0, isHV = 0, dirHV = 0;
-	int noBytesByRow = 2*noPEs/8;
-	int noIntRegsByRow = noBytesByRow/sizeof(unsigned int);
-	int arrowRow[2*noBytesByRow];
-	if (dimFile % (2*noPEs/8) == 0) sizeValid = 1;
-	if (sizeValid==1 && dimFile>noBytesByRow){
-		fseek(inputFile, dimFile-noBytesByRow, SEEK_SET);
-		fread(&arrowRow,4,noIntRegsByRow,inputFile);
-			readRow = 1;
-	}
-	for (int i=0;i<noIntRegsByRow;i++){
-		if (arrowRow[i] == dirHcode) isHV++;
-		if (arrowRow[i] == dirVcode) isHV--;
-	}
-	if (isHV == noIntRegsByRow) dirHV = 1;	//H
-	if (isHV == -noIntRegsByRow) dirHV = -1;	//V
-	if (dirHV == 0) return 0;
-	else return dirHV;
-}
-
 int getNoArrowRows(int dimFile, int noPEs) {
 	return dimFile / (2*noPEs/8);
 }
+
+int getLastDirection(FILE *inputFile, int dimFile, int noPEs, int *arrowRow) {
+	const int dirHcode = 1431655765, dirVcode = 2863311530;
+	int sizeValid = 0, isRead = 0, isHV = 0, dirHV = 0;
+	int noBytesByRow = 2*noPEs/8, dimUInt=sizeof(unsigned int);
+	int noIntRegsByRow = noBytesByRow/dimUInt;
+	if (dimFile % (2*noPEs/8) == 0)
+		sizeValid = 1;
+	if (sizeValid==1 && dimFile>noBytesByRow){
+		fseek(inputFile, dimFile-1*noBytesByRow, SEEK_SET);
+		fread(arrowRow,4,noIntRegsByRow,inputFile);
+		for (int i=0;i<noIntRegsByRow;i++){
+			if (arrowRow[i] == dirHcode) isHV++;
+			if (arrowRow[i] == dirVcode) isHV--;
+		}
+		isRead = 1;
+	}
+	if (sizeValid==1 && isRead==1){
+		if (isHV == noIntRegsByRow) dirHV = 1;		//H
+		if (isHV == -noIntRegsByRow) dirHV = -1;	//V
+	}
+	if (dirHV == 0) return 0;
+	else return dirHV;
+	
+}
+
+int getPosLastArrow(FILE *inputFile, int dimFile, int noPEs, int *arrowRow) {
+	int i=0, j=0, dimUInt=sizeof(unsigned int), noBitsUInt=8*dimUInt;
+	int noBytesByRow = 2*noPEs/8, noUIntRegsByRow = noBytesByRow/dimUInt;
+	int noIntRegsByRow = noBytesByRow/dimUInt;
+	int dupla=0, noArrows=0, posLastArrow=-1;
+	//ARROW = 0 @ ERROR, 1 @ UP, 2 @ LEFT, 3 @ DIAGONAL
+	fseek(inputFile, -2*noBytesByRow, SEEK_CUR);
+	fread(arrowRow,dimUInt,noIntRegsByRow,inputFile);
+	for (i=0; i<noUIntRegsByRow; i++){
+		for (j=0; j<noBitsUInt/2; j++){
+			dupla = (arrowRow[i]>>2*j) & 3;
+			if (dupla != 0) {
+				noArrows++;
+				posLastArrow = i*8*dimUInt + 2*j;
+			}
+		}
+	}
+	return posLastArrow;
+}
+
+int getArrow(int noPEs, int *arrowRow, int posArrow, int dirArrow){
+	int dimUInt=sizeof(unsigned int);
+	int posInRow = posArrow / (8*dimUInt);
+	int posInReg = (posArrow % (8*dimUInt));
+	int ARROW = (arrowRow[posInRow]>>posInReg) & 3;
+	return ARROW;
+}
+
 
 
 void decoArrow(int* offsetPos, int* offsetFila, int* dirHV, int ARROW, int* posSeqA, int* posSeqB, int* gapA, int* gapB) {
